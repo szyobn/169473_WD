28d19b10bb5cd65bbb2f1a54cbc574367dea69be
szyobn <115112673+szyobn@users.noreply.github.com>
2023-03-08 19:18:38 +0100

    lekcja1

--- main.py
+++ main.py
@@ -1,16 +1,102 @@
-# This is a sample Python script.
+# a = 'a'
+# print(a)
+#
+# #dodanie elementu na pozycje
+# zad1 = ['ala','basia','ula']
+# zad1.append('ewa')
+# print(zad1)
+# #dodanie kilku elementów na koniec listy
+# zad2 = [1,2,3]
+# test = [4,5,6]
+# zad2.insert(3,test)
+# print(zad2)
+# #usuwanie elemenntu po indeksie
+# zad3 = [1,2,3]
+# zad3.pop(1)
+# print(zad3)
+# #usuwanie elementu po wartości elementu
+# zad4 = [1,2,3]
+# zad4.remove(1)
+# print(zad4)
+# #odwórcenie listy
+# zad5 = ['ala','elwelina','basia','lili']
+# zad5.reverse()
+# print(zad5)
+# #sortowanie
+# zad6 = ['ala','elwelina','basia','lili']
+# zad6.sort()
+# print(zad6)
+#
+# slownik = {'a':1,3:1,5:'b','a':5}
+# print(slownik)
+# print(slownik['a'])
+# print(slownik.keys())
+# print(slownik.values())
+#
+# print('a =%(zm)d'%{'zm':12})
+# print('a = {}'.format(1))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#instrukcja warunkowa if :
+#   instrukcja 1
+#   instrukcja 2
+# elif warunek 2:
+#   instrukcja 1
+#   instrukcja 2
+#else:
+#   instrukcja 1
+#   instrukcja 2
+# a = input('podaj a: ')
+# b = input('podaj b: ')
+# print(a)
+# print(b)
+# print(type(a))
+
+#if a>b:
+ #   print('a= '+str(a))
+#elif a < b:
+ #       print('b= ' + str(b))
+#else:
+ #   print("a i b sa rowne")
+
+#for element in sekwencja:
+#   instrukcja 1
+#   instrukcja 2
+#else:
+#   instrukcja 1
+#   instrukcja 2
+#for x in range(1,6,1):
+#    print(x)
+ #   print("")
+
+#range(start,stop,step) for(int i =0, i < lista.count(), i++)
+
+#while element in sekwencja:
+#   instrukcja 1
+#   instrukcja 2
+#else:
+#   instrukcja 1
+#   instrukcja 2
+#
+
+# liczby = [3,45,3,2,1]
+# licznik = 0
+# a = int(input('podaj a: '))
+# while licznik != len(liczby):
+#     if a - liczby[licznik] == 0:
+#         print('{} - {} = 0'.format(a, liczby[licznik]))
+#         break
+#         licznik +=1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+liczby = [1,2,2,2,2,3]
+licznik = 0;
 
+while licznik != len(liczby):
+     if liczby[licznik] == 2:
+         liczby.remove(liczby[licznik])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+     else:
+         licznik +=1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(liczby)
